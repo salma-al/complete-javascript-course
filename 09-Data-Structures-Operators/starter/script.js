45f72eeec64c138f2fa89f85cfd4627c0b7881fe
salma-al <salma_fouad@outlook.com>
2023-08-16 03:23:11 +0300

    section 9 - challenge 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,96 @@ const restaurant = {
     },
   },
 };
+
+// 2) Functions
+const add = function (...numbers) {
+  // console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  // console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Q1
+const goalNames = Object.entries(game.scored);
+// console.log(goalNames);
+for (const [number, name] of goalNames) {
+  console.log(`goal ${+number + 1}: ${name}`);
+}
+
+// Q2
+const odds = Object.values(game.odds);
+// console.log(odds);
+let avgOdd = 0;
+for (const x of odds) {
+  avgOdd += x;
+}
+
+console.log(avgOdd / odds.length);
+
+// Q3
+const oddKeys = Object.entries(game.odds);
+
+for (const [key, val] of oddKeys) {
+  console.log(
+    game?.[key] ? `odds of victory ${key}: ${val}` : `odds of draw: ${val}`
+  );
+}
+
+// Q4
+
+const scoreArr = game.scored;
+
+const scorers = {};
+for (const el of scoreArr) {
+  scorers[el] ? ++scorers[el] : (scorers[el] = +1);
+}
+
+console.log(scorers);
